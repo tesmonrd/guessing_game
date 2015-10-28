0f7e3133de60aa061a09eb0813f3eb7e9a3fc6f6
Rick Tesmond <tesmonrd@gmail.com>
2015-10-28 15:25:01 -0700

    Reformatted document, added Q5

--- guessinggameJS.js
+++ guessinggameJS.js
@@ -1,56 +1,87 @@
+
+var ans1, ans2, ans3, ans4, ans5;
+
 var name = prompt("Please enter your name.");
-alert("Hey there, " + name +"! Please answer the questions with a 'Yes' or 'No'.");
 
-  var q1= prompt('Is Rick from Seattle, Washington?');
-    if(q1 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
-      alert("Wrong! He is from Cincinnati, Ohio.");
-      q1=0;
-      console.log('The user answered incorrectly with' + q1);
-    } else {
-      alert("Correct! He is from Cincinnati, Ohio");
-      q1=1;
-      console.log('The user answered correctly with' + q1);
-    };
+alert("Hey there, " + name + "! Please answer the questions with a 'Yes' or 'No'.");
 
-  var q2= prompt("Did Rick graduate college with a degree in psychology?");
-    if(q2 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
-      alert("Correct! He specialized in research psychology.");
-      q2=1;
-      console.log('The user answered correctly with' + q2);
-    } else {
-      alert("Wrong! He majored in psychology with an emphasis on research.");
-      q2=0;
-      console.log('The user answered incorrectly with' + q2);
-    };
+var q1 = prompt('Is Rick from Seattle, Washington?');
 
-  var q3= prompt("Did Rick live in Denmark?");
+if(q1.toLowerCase() === "yes") {
+  alert("Wrong! He is from Cincinnati, Ohio.");
+  ans1 = 0;
+  console.log('The user answered incorrectly with ' + q1);
+} else if(q1.toLowerCase() === "no") {
+  alert("Correct! He is from Cincinnati, Ohio");
+  ans1=1;
+  console.log('The user answered correctly with ' + q1);
+} else {
+  alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+  ans1 = 0;
+}
 
-    if(q3 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
-      alert("Correct! He lived in a town called Hareskov for 8 months.");
-        q3=1;
-        console.log('The user answered correctly with' + q3);
-    } else{
-        alert("Wrong! He lived in a town called Hareskov for 8 months");
-        q3=0;
-        console.log('The user answered incorrectly with' + q3);
-      };
+var q2 = prompt("Did Rick graduate college with a degree in psychology?");
 
-  var q4= prompt("Is Rick a 'dog person'?");
+if(q2.toLowerCase() === "yes") {
+  alert("Correct! He specialized in research psychology.");
+  ans2 = 1;
+  console.log('The user answered correctly with ' + q2);
+} else if(q2.toLowerCase() === "no") {
+  alert("Wrong! He majored in psychology with an emphasis on research.");
+  ans2 = 0;
+  console.log('The user answered incorrectly with ' + q2);
+} else {
+  alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+  ans2 = 0;
+}
 
-    if(q4 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
-      alert("Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.");
-        q4=0;
-        console.log('The user answered incorrectly with ' + q4);
-    } else{
-        alert("Correct, he is a self proclaimed 'cat person'.");
-        q4=1;
-        console.log('The user answered correctly with ' + q4);
-         };
+var q3 = prompt("Did Rick live in Denmark?");
 
-  var score= q1 + q2 + q3;
+if(q3 === "yes") {
+  alert("Correct! He lived in a town called Hareskov for 8 months.");
+  ans3 = 1;
+  console.log('The user answered correctly with ' + q3);
+}   else if(q3.toLowerCase() === "no") {
+  alert("Wrong! He lived in a town called Hareskov for 8 months");
+  ans3 = 0;
+  console.log('The user answered incorrectly with ' + q3);
+} else {
+  alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+  ans3 = 0;
+}
 
-    if(score < 3) {
-      alert("You got "+ score + " out of 3 questions correct! Looks like you need to get to know Rick a bit more!");
-    } else{
-      alert("You got a perfect score, " + score + " out of 3 right! You know Rick pretty well!");
-    };
+var q4 = prompt("Is Rick a 'dog person'?");
+
+if(q4 === "yes") {
+  alert("Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.");
+  ans4 = 0;
+  console.log('The user answered incorrectly with ' + q4);
+} else if(q4.toLowerCase() === "no") {
+  alert("Correct, he is a self proclaimed 'cat person'.");
+  ans4 = 1;
+  console.log('The user answered correctly with ' + q4);
+} else {
+  alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+  ans4 = 0;
+}
+
+var q5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
+
+while (q5 !== 9) {
+  alert("Not quite right... try again (hint: It's between 5 and 10");
+  console.log('The user answered incorrectly with ' + q5);
+  q5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
+}
+
+alert("Correct! I'm not sure how you knew that; that's impressive");
+console.log('The user answered correctly with ' + q5);
+ans5 = 1;
+
+
+var score = ans1 + ans2 + ans3 + ans4 + ans5;
+
+if(score < 5) {
+  alert("You got " + score + " out of 5 questions correct! Looks like you need to get to know Rick a bit more!");
+} else{
+  alert("You got a perfect score, " + score + " out of 5 right! You know Rick pretty well!");
+}
