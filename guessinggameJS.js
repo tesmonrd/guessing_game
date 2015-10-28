077974180a131dad4a5b84591946a8ec551b6aaf
Rick Tesmond <tesmonrd@gmail.com>
2015-10-28 14:01:04 -0700

    Added fifth question and error control

--- guessinggameJS.js
+++ guessinggameJS.js
@@ -2,7 +2,7 @@ var name = prompt("Please enter your name.");
 alert("Hey there, " + name +"! Please answer the questions with a 'Yes' or 'No'.");
 
   var q1= prompt('Is Rick from Seattle, Washington?');
-    if(q1.toLowerCase() === "yes") {
+    if(q1 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
       alert("Wrong! He is from Cincinnati, Ohio.");
       q1=0;
       console.log('The user answered incorrectly with' + q1);
@@ -13,7 +13,7 @@ alert("Hey there, " + name +"! Please answer the questions with a 'Yes' or 'No'.
     };
 
   var q2= prompt("Did Rick graduate college with a degree in psychology?");
-    if(q2.toLowerCase() === "yes") {
+    if(q2 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
       alert("Correct! He specialized in research psychology.");
       q2=1;
       console.log('The user answered correctly with' + q2);
@@ -25,15 +25,27 @@ alert("Hey there, " + name +"! Please answer the questions with a 'Yes' or 'No'.
 
   var q3= prompt("Did Rick live in Denmark?");
 
-    if(q3.toLowerCase() === "yes") {
+    if(q3 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
       alert("Correct! He lived in a town called Hareskov for 8 months.");
-      q3=1;
-      console.log('The user answered correctly with' + q3);
+        q3=1;
+        console.log('The user answered correctly with' + q3);
     } else{
         alert("Wrong! He lived in a town called Hareskov for 8 months");
-      q3=0;
-      console.log('The user answered incorrectly with' + q3);
+        q3=0;
+        console.log('The user answered incorrectly with' + q3);
       };
+
+  var q4= prompt("Is Rick a 'dog person'?");
+
+    if(q4 === "yes" || "Yes" || "Yeya" || "Yessuh" || "yeya") {
+      alert("Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.");
+        q4=0;
+        console.log('The user answered incorrectly with ' + q4);
+    } else{
+        alert("Correct, he is a self proclaimed 'cat person'.");
+        q4=1;
+        console.log('The user answered correctly with ' + q4);
+         };
 
   var score= q1 + q2 + q3;
 
