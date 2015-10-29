17eb690958c92b9181e7b6e7cedd9bec5723cc2a
Miranda Raymond <mirandaraymond@me.com>
2015-10-29 13:29:58 -0700

    changed all the alerts to variables

--- game.js
+++ game.js
@@ -1,23 +1,31 @@
 
 var ans1, ans2, ans3, ans4, ans5, ans6;
 
+var qa1 = document.getElementById('answer1');
+var qa2 = document.getElementById('answer2');
+var qa3 = document.getElementById('answer3');
+var qa4 = document.getElementById('answer4');
+var qa5 = document.getElementById('answer5');
+var qa6 = document.getElementById('answer6');
+var qa7 = document.getElementById('answer7');
+
 var name = prompt("Please enter your name.");
 
-alert("Hey there, " + name + "! Please answer the questions with a 'Yes' or 'No'.");
+qa1.innerHTML = "Hey there, " + name + "! Please answer the questions with a 'Yes' or 'No'.";
 
 function questionOne() {
   var q1 = prompt('Is Rick from Seattle, Washington?');
 
   if(q1.toLowerCase() === "yes" || "y" || "yeah") {
-    alert("Wrong! He is from Cincinnati, Ohio.");
+    qa2.innerHTML = "Wrong! He is from Cincinnati, Ohio.";
     ans1 = 0;
     console.log('The user answered incorrectly with ' + q1);
   } else if(q1.toLowerCase() === "no" || "n" || "naw") {
-    alert("Correct! He is from Cincinnati, Ohio");
+    qa2.innerHTML = "Correct! He is from Cincinnati, Ohio";
     ans1=1;
     console.log('The user answered correctly with ' + q1);
   } else {
-    alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+    qa2.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     ans1 = 0;
   }
 };
@@ -26,15 +34,15 @@ function questionTwo() {
   var q2 = prompt("Did Rick graduate college with a degree in psychology?");
 
   if(q2.toLowerCase() === "yes" || "y" || "yeah") {
-    alert("Correct! He specialized in research psychology.");
+    qa3.innerHTML = "Correct! He specialized in research psychology.";
     ans2 = 1;
     console.log('The user answered correctly with ' + q2);
   } else if(q2.toLowerCase() === "no" || "n" || "naw") {
-    alert("Wrong! He majored in psychology with an emphasis on research.");
+    qa3.innerHTML = "Wrong! He majored in psychology with an emphasis on research.";
     ans2 = 0;
     console.log('The user answered incorrectly with ' + q2);
   } else {
-    alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+    qa3.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     ans2 = 0;
   }
 };
@@ -43,15 +51,15 @@ function questionThree() {
   var q3 = prompt("Did Rick live in Denmark?");
 
   if(q3 === "yes" || "y" || "yeah") {
-    alert("Correct! He lived in a town called Hareskov for 8 months.");
+    qa4.innerHTML = "Correct! He lived in a town called Hareskov for 8 months.";
     ans3 = 1;
     console.log('The user answered correctly with ' + q3);
   }   else if(q3.toLowerCase() === "no" || "n" || "naw") {
-    alert("Wrong! He lived in a town called Hareskov for 8 months");
+    qa4.innerHTML = "Wrong! He lived in a town called Hareskov for 8 months";
     ans3 = 0;
     console.log('The user answered incorrectly with ' + q3);
   } else {
-    alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+    qa4.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     ans3 = 0;
   }
 };
@@ -60,15 +68,15 @@ function questionFour() {
   var q4 = prompt("Is Rick a 'dog person'?");
 
   if(q4 === "yes" || "y" || "yeah") {
-    alert("Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.");
+    qa5.innerHTML = "Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.";
     ans4 = 0;
     console.log('The user answered incorrectly with ' + q4);
   } else if(q4.toLowerCase() === "no" || "n" || "naw") {
-    alert("Correct, he is a self proclaimed 'cat person'.");
+    qa5.innerHTML = "Correct, he is a self proclaimed 'cat person'.";
     ans4 = 1;
     console.log('The user answered correctly with ' + q4);
   } else {
-    alert("That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.");
+    qa5.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     ans4 = 0;
   }
 };
@@ -78,13 +86,13 @@ function questionFive() {
   var q5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
 
   while (q5 !== 9) {
-    alert("Not quite right... try again (hint: It's between 5 and 10)");
+    qa6.innerHTML = "Not quite right... try again (hint: It's between 5 and 10)";
     console.log('The user answered incorrectly with ' + q5);
     q5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
     ans5 = 0
   }
 
-  alert("Correct! I'm not sure how you knew that; that's impressive");
+  qa6.innerHTML = "Correct! I'm not sure how you knew that; that's impressive";
   console.log('The user answered correctly with ' + q5);
   ans5 = 1;
 };
@@ -93,13 +101,13 @@ function questionSix() {
   var q6 = parseInt(prompt("How old is Rick?"));
 
   while (q6 !== 22) {
-    alert("Not quite right...try again (hint: It's between 20 and 24)");
+    qa7.innerHTML = "Not quite right...try again (hint: It's between 20 and 24)";
     console.log('The user answered incorrectly with ' + q6);
     q6 = parseInt(prompt("How old is Rick?"));
     ans6 = 0
   }
 
-  alert("Correct! I'm not sure how you knew that; that's impressive");
+  qa7.innerHTML = "Correct! I'm not sure how you knew that; that's impressive";
     console.log('The user answered correctly with ' + q6);
     ans6 = 1;
 };
