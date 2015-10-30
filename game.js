59035a71771654ce390cd90259d68fe1b0d475b0
Sawako Ishida <sawako.yoriko@gmail.com>
2015-10-30 14:37:09 -0700

    added css and class element to js

--- game.js
+++ game.js
@@ -21,14 +21,17 @@ function questionOne() {
 
   if(quest1.toLowerCase() === "yes" || quest1.toLowerCase() === "y" || quest1.toLowerCase() === "yeah") {
     ans2.innerHTML = "Wrong! He is from Cincinnati, Ohio.";
+    ans2.className = "red";
     value1 = 0;
     console.log('The user answered incorrectly with ' + quest1);
   } else if(quest1.toLowerCase() === "no" || quest1.toLowerCase() === "n" || quest1.toLowerCase() === "naw") {
     ans2.innerHTML = 'Correct! He is from Cincinnati, Ohio <br /> <img style= "width: 200px;" src="images/Cincinnati-Bengals-Logo.gif" alt="Cincy"/>';
+    ans2.className = "green";
     value1 = 1;
     console.log('The user answered correctly with ' + quest1);
   } else {
     ans2.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
+    ans2.className = "red";
     value1 = 0;
   }
 }
@@ -38,14 +41,17 @@ function questionTwo() {
 
   if(quest2.toLowerCase() === "yes" || quest2.toLowerCase() === "y" || quest2.toLowerCase() === "yeah") {
     ans3.innerHTML = 'Correct! He specialized in research psychology. <br /> <img style= "width: 200px;" src="images/Whitman.jpg" alt="College"/>';
+    ans3.className = "green";
     value2 = 1;
     console.log('The user answered correctly with ' + quest2);
   } else if(quest2.toLowerCase() === "no" || quest2.toLowerCase() === "n" || quest2.toLowerCase() === "naw") {
     ans3.innerHTML = "Wrong! He majored in psychology with an emphasis on research.";
+    ans3.className = "red";
     value2 = 0;
     console.log('The user answered incorrectly with ' + quest2);
   } else {
     ans3.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
+    ans3.className = "red";
     value2 = 0;
   }
 }
@@ -55,14 +61,17 @@ function questionThree() {
 
   if(quest3.toLowerCase() === "yes" || quest3.toLowerCase() === "y" || quest3.toLowerCase() === "yeah") {
     ans4.innerHTML = 'Correct! He lived in a town called Hareskov for 8 months. <br /> <img style= "width: 200px;" src="images/denmark-01.jpg" alt="Denmark"/>';
+    ans4.className = "green";
     value3 = 1;
     console.log('The user answered correctly with ' + quest3);
   }   else if(quest3.toLowerCase() === "no" || quest3.toLowerCase() === "n" || quest3.toLowerCase() === "naw") {
     ans4.innerHTML = "Wrong! He lived in a town called Hareskov for 8 months";
+    ans4.className = "red";
     value3 = 0;
     console.log('The user answered incorrectly with ' + quest3);
   } else {
     ans4.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
+    ans4.className = "red";
     value3 = 0;
   }
 }
@@ -72,14 +81,17 @@ function questionFour() {
 
   if(quest4.toLowerCase() === "yes" || quest4.toLowerCase() === "y" || quest4.toLowerCase() === "yeah") {
     ans5.innerHTML = "Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.";
+    ans5.className = "red";
     value4 = 0;
     console.log('The user answered incorrectly with ' + quest4);
   } else if(quest4.toLowerCase() === "no" || quest4.toLowerCase() === "n" || quest4.toLowerCase() === "naw") {
     ans5.innerHTML = "Correct, he is a self proclaimed cat person. <br /> <img style= 'width: 200px;' src='images/cat.jpg' alt='Cat'/>";
+    ans5.className = "green";
     value4 = 1;
     console.log('The user answered correctly with ' + quest4);
   } else {
     ans5.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
+    ans5.className = "red";
     value4 = 0;
   }
 }
@@ -96,6 +108,7 @@ function questionFive() {
 
   ans6.innerHTML = 'Correct! Have a free point! <br /> <img style= "width: 200px;" src="images/number9.png" alt="Number"/>';
   console.log('The user answered correctly with ' + quest5);
+  ans6.className = "green";
   value5 = 1;
 }
 
@@ -110,6 +123,7 @@ function questionSix() {
 
   ans7.innerHTML = 'Correct! Have a free point! <br /> <img style= "width: 200px;" src="images/twentytwo.jpg" alt="22"/>';
     console.log('The user answered correctly with ' + quest6);
+    ans7.className = "green";
     value6 = 1;
 }
 
