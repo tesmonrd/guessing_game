6b499eb756a84d4af4f7ecf1f98515e9d155c4e9
Rick Tesmond <tesmonrd@gmail.com>
2015-10-30 11:23:07 -0700

    further error corrections, syntax corrections

--- game.js
+++ game.js
@@ -14,16 +14,16 @@ var name = prompt("Please enter your name.");
 ans1.innerHTML = "Hey there, " + name + "! Please answer the questions with a 'Yes' or 'No'.";
 
 function questionOne() {
-  var q1 = prompt('Is Rick from Seattle, Washington?');
+  var quest1 = prompt('Is Rick from Seattle, Washington?');
 
-  if(q1.toLowerCase() === "yes" || q1.toLowerCase() === "y" || q1.toLowerCase() === "yeah") {
+  if(quest1.toLowerCase() === "yes" || quest1.toLowerCase() === "y" || quest1.toLowerCase() === "yeah") {
     ans2.innerHTML = "Wrong! He is from Cincinnati, Ohio.";
     value1 = 0;
-    console.log('The user answered incorrectly with ' + q1);
-  } else if(q1.toLowerCase() === "no" || q1.toLowerCase() === "n" || q1.toLowerCase() === "naw") {
+    console.log('The user answered incorrectly with ' + quest1);
+  } else if(quest1.toLowerCase() === "no" || quest1.toLowerCase() === "n" || quest1.toLowerCase() === "naw") {
     ans2.innerHTML = "Correct! He is from Cincinnati, Ohio";
     value1 = 1;
-    console.log('The user answered correctly with ' + q1);
+    console.log('The user answered correctly with ' + quest1);
   } else {
     ans2.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     value1 = 0;
@@ -31,16 +31,16 @@ function questionOne() {
 }
 
 function questionTwo() {
-  var q2 = prompt("Did Rick graduate college with a degree in psychology?");
+  var quest2 = prompt("Did Rick graduate college with a degree in psychology?");
 
-  if(q2.toLowerCase() === "yes" || q2.toLowerCase() === "y" || q2.toLowerCase() === "yeah") {
+  if(quest2.toLowerCase() === "yes" || quest2.toLowerCase() === "y" || quest2.toLowerCase() === "yeah") {
     ans3.innerHTML = "Correct! He specialized in research psychology.";
     value2 = 1;
-    console.log('The user answered correctly with ' + q2);
-  } else if(q2.toLowerCase() === "no" || q2.toLowerCase() === "n" || q2.toLowerCase() === "naw") {
+    console.log('The user answered correctly with ' + quest2);
+  } else if(quest2.toLowerCase() === "no" || quest2.toLowerCase() === "n" || quest2.toLowerCase() === "naw") {
     ans3.innerHTML = "Wrong! He majored in psychology with an emphasis on research.";
     value2 = 0;
-    console.log('The user answered incorrectly with ' + q2);
+    console.log('The user answered incorrectly with ' + quest2);
   } else {
     ans3.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     value2 = 0;
@@ -48,16 +48,16 @@ function questionTwo() {
 }
 
 function questionThree() {
-  var q3 = prompt("Did Rick live in Denmark?");
+  var quest3 = prompt("Did Rick live in Denmark?");
 
-  if(q3.toLowerCase() === "yes" || q3.toLowerCase() === "y" || q3.toLowerCase() === "yeah") {
+  if(quest3.toLowerCase() === "yes" || quest3.toLowerCase() === "y" || quest3.toLowerCase() === "yeah") {
     ans4.innerHTML = "Correct! He lived in a town called Hareskov for 8 months.";
     value3 = 1;
-    console.log('The user answered correctly with ' + q3);
-  }   else if(q3.toLowerCase() === "no" || q3.toLowerCase() === "n" || q3.toLowerCase() === "naw") {
+    console.log('The user answered correctly with ' + quest3);
+  }   else if(quest3.toLowerCase() === "no" || quest3.toLowerCase() === "n" || quest3.toLowerCase() === "naw") {
     ans4.innerHTML = "Wrong! He lived in a town called Hareskov for 8 months";
     value3 = 0;
-    console.log('The user answered incorrectly with ' + q3);
+    console.log('The user answered incorrectly with ' + quest3);
   } else {
     ans4.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     value3 = 0;
@@ -65,16 +65,16 @@ function questionThree() {
 }
 
 function questionFour() {
-  var q4 = prompt("Is Rick a 'dog person'?");
+  var quest4 = prompt("Is Rick a 'dog person'?");
 
-  if(q4.toLowerCase() === "yes" || q4.toLowerCase() === "y" || q4.toLowerCase() === "yeah") {
+  if(quest4.toLowerCase() === "yes" || quest4.toLowerCase() === "y" || quest4.toLowerCase() === "yeah") {
     ans5.innerHTML = "Wrong! He is a 'cat person', but is very open to becomming a 'dog person'.";
     value4 = 0;
-    console.log('The user answered incorrectly with ' + q4);
-  } else if(q4.toLowerCase() === "no" || q4.toLowerCase() === "n" || q4.toLowerCase() === "naw") {
+    console.log('The user answered incorrectly with ' + quest4);
+  } else if(quest4.toLowerCase() === "no" || quest4.toLowerCase() === "n" || quest4.toLowerCase() === "naw") {
     ans5.innerHTML = "Correct, he is a self proclaimed 'cat person'.";
     value4 = 1;
-    console.log('The user answered correctly with ' + q4);
+    console.log('The user answered correctly with ' + quest4);
   } else {
     ans5.innerHTML = "That is not a valid answer... moving on. Remember answer with a 'yes' or 'no'.";
     value4 = 0;
@@ -83,32 +83,30 @@ function questionFour() {
 
 
 function questionFive() {
-  var q5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
+  var quest5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
 
-  while (q5 !== 9) {
+  while (quest5 !== 9) {
     ans6.innerHTML = "Not quite right... try again (hint: It's between 5 and 10)";
-    console.log('The user answered incorrectly with ' + q5);
-    q5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
-    value5 = 0
+    console.log('The user answered incorrectly with ' + quest5);
+    quest5 = parseInt(prompt("What is Rick's lucky number? ANSWER WITH A NUMBER!"));
   }
 
-  ans6.innerHTML = "Correct! I'm not sure how you knew that; that's impressive!";
-  console.log('The user answered correctly with ' + q5);
+  ans6.innerHTML = "Correct! Have a free point!";
+  console.log('The user answered correctly with ' + quest5);
   value5 = 1;
 }
 
 function questionSix() {
-  var q6 = parseInt(prompt("How old is Rick?"));
+  var quest6 = parseInt(prompt("How old is Rick?"));
 
-  while (q6 !== 22) {
+  while (quest6 !== 22) {
     ans7.innerHTML = "Not quite right...try again (hint: It's between 20 and 24)";
-    console.log('The user answered incorrectly with ' + q6);
-    q6 = parseInt(prompt("How old is Rick?"));
-    value6 = 0
+    console.log('The user answered incorrectly with ' + quest6);
+    quest6 = parseInt(prompt("How old is Rick?"));
   }
 
-  ans7.innerHTML = "Correct! I'm not sure how you knew that; that's impressive!";
-    console.log('The user answered correctly with ' + q6);
+  ans7.innerHTML = "Correct! Have a free point!";
+    console.log('The user answered correctly with ' + quest6);
     value6 = 1;
 }
 
